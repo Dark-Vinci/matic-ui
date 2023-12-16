d0f451edd9739cf087c425f415c966b0a0a08af1
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-12-16 17:56:01 +0100

    chore: more work to be done

---
+++ src/components/Body/VideoCard/VideoCard.tsx
@@ -0,0 +1,33 @@
+import { JSX } from 'react';
+
+import style from './VideoCard.module.scss';
+
+interface VideoCardProps {
+    readonly children: JSX.Element;
+    readonly url: string;
+    readonly type: string;
+}
+
+export function VideoCard({ url, type, children }: VideoCardProps): JSX.Element {
+    console.log({url})
+    return (
+        <div className={ style.container}>
+            <div className={ style.video_container }>
+                <div className={ style.vid_container }>
+                    <video 
+                        muted 
+                        controls={false} 
+                        autoPlay 
+                        loop
+                    >
+                        <source src={url} type={type} />
+                    </video>
+                </div>
+
+                <div className={ style.children_container }>
+                    { children }
+                </div>
+            </div>
+        </div>
+    );
+}
